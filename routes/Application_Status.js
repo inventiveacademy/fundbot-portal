2124723a85c2015f466ab13894285be749b38f5f
Drake Siemer <dsiemer@gmail.com>
2017-09-06 09:57:54 -0500

    one sec

--- routes/Application_Status.js
+++ routes/Application_Status.js
@@ -5,9 +5,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	    //request('http://localhost:3001/applicants', function(error, response, body) {
-       // let Applicants = JSON.parse(body);
-        res.render('Application_Status', { title: 'Application Status' /*Applicants */});
+    request('http://localhost:3008/applications', function(error, response, body) {
+        let applications = JSON.parse(body);
+        res.render('Application_Status', { title: 'Applitcation Status', applications });
+    });
 });
 
 
--- routes/Payment_Configuration.js
+++ routes/Payment_Configuration.js
@@ -5,9 +5,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	    //request('http://localhost:3001/applicants', function(error, response, body) {
-       // let Applicants = JSON.parse(body);
-        res.render('Payment_Configuration', { title: 'Payment Configuration' /*Applicants */});
+    request('http://localhost:3008/applications', function(error, response, body) {
+        let applications = JSON.parse(body);
+        res.render('Payment_Configuration', { title: 'Payment Configuration', applications });
+    });
 });
 
 
