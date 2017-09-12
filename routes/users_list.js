e11447fd70d88676e87883211592627669063cb8
Drake Siemer <dsiemer@gmail.com>
2017-09-12 11:31:55 -0500

    routes and style sheets work and we are extending from layout

---
+++ routes/users_list.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var app = express();
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    request('http://localhost:3008/applications', function(error, response, body) {
+        let applications = JSON.parse(body);
+        res.render('users_list', { title: 'admins list', applications });
+    });
+});
+
+
+module.exports = router;
