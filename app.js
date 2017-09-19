9be60dbf25171ee0756030f42d7aaaf1e6d2d4e2
Trinity Siemer <tsiemer@gmail.com>
2017-09-18 21:22:31 -0500

    Added logout!!

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var session = require('express-session');
 var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
 
-var Login = require('./routes/Login');
+var Login = require('./routes/login');
+var logout = require('./routes/logout');
 var loginError = require('./routes/login-error');
 var password_reset = require('./routes/password_reset');
 var password_sent = require('./routes/password_sent');
@@ -67,6 +68,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/Login', Login);
+app.use('/logout', logout);
 app.use('/login-error', loginError);
 app.use('/password_reset', password_reset);
 app.use('/password_sent' , password_sent);
