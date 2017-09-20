6fb8c724954593b7fd38a98f583976b0cf435d60
Kevin <kevin.a.wilde@gmail.com>
2017-09-19 19:56:27 -0500

    sorted overview page

--- routes/Applications_Overview.js
+++ routes/Applications_Overview.js
@@ -7,6 +7,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     request('http://localhost:3008/applications', function(error, response, body) {
         let applications = JSON.parse(body);
+        applications.sort();
         res.render('Applications_Overview', { title: 'Applications Overview', applications });
     });
 });
