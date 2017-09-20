dad79fc6e320b4a0e754a6a93702ed4c50ab246c
Trinity Siemer <tsiemer@gmail.com>
2017-09-20 17:51:29 -0500

    adding session to login

--- routes/login.js
+++ routes/login.js
@@ -18,6 +18,8 @@ router.post('/', function(req, res, next) {
     if (req.body.email && req.body.password) {
         request({ url: `http://localhost:3008/login?user=${req.body.email}&pwd=${req.body.password}`, method: "POST", datatype:"json"}, function(error, response, body) {
                 let success = JSON.parse(body);
+        request({ url: `http://localhost:3008/login?user=${req.body.email}&pwd=${req.body.password}`, method: "POST", json:{"user": req.body.email, "pwd":req.body.password}}, function(error, response, body) {
+                let success = body;
                 let applicant;
                 console.log(body);
                 console.log(success);
