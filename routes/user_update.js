b901058168adf7fc6bdb7ed1a628cd337086a0b3
Trinity Siemer <tsiemer@gmail.com>
2017-09-19 20:18:10 -0500

    Solved merge conflicts

--- routes/user_update.js
+++ routes/user_update.js
@@ -2,10 +2,8 @@ var express = require('express');
 var request = require('request');
 var app = express();
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    request('http://localhost:3008/applications', function(error, response, body) {
     request('http://localhost:3008/users_update', function(error, response, body) {
         let applications = JSON.parse(body);
         res.render('user_update', { title: 'Users Update', applications });
