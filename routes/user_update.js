e430cd106867ba44d2992004960f835bfa5b72b5
Trinity Siemer <tsiemer@gmail.com>
2017-09-19 21:32:12 -0500

    Bug fix / User update

--- routes/user_update.js
+++ routes/user_update.js
@@ -3,19 +3,20 @@ var request = require('request');
 var app = express();
 var router = express.Router();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    request('http://localhost:3008/users_update', function(error, response, body) {
-        let applications = JSON.parse(body);
-        res.render('user_update', { title: 'Users Update', applications });
-        console.log("Rendering Users Update Page!!!>!@")
-    });
+router.get('/:id', function(req, res, next){
+	request(`http://localhost:3008/applications/${req.params.id}`, function(error, response, body){
+		let applications = JSON.parse(body);
+		res.render('user_update', { title: 'Users Update', applications});
+		console.log("Rendering Users Update Page!!!>!@");
+	});
 });
+
 router.put('/', function(req, res, next){
 	request('/applications/:id', function(error, response, body) {
 	    let applications = JSON.parse(body);
 	    console.log("Happy Update!!!")
 	});
-})
+});
 
 
 module.exports = router;
