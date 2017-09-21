681017507402a07df977274ffaa9bde4e49fcdd2
Trinity Siemer <tsiemer@gmail.com>
2017-09-21 14:16:56 -0500

    Working logout!

--- routes/logout.js
+++ routes/logout.js
@@ -7,12 +7,12 @@ var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose'); 
 var mid = require('../middleware'); 
  
-/* GET home page. */ 
 router.get('/', function(req, res, next) { 
     request('/logout/:user', function(error, response, body) { 
         console.log("Called Ron's api logout function!");
     }); 
     console.log("Logout was called!!"); 
+    console.log(req.session); 
     req.session.applicantId = undefined; 
     res.redirect("/login"); 
 }); 
