c43f113a0b159697ae051cc4350bd801843774da
Kevin <kevin.a.wilde@gmail.com>
2017-09-20 19:22:47 -0500

    tabs again

--- public/javascripts/application-overview.js
+++ public/javascripts/application-overview.js
@@ -38,4 +38,15 @@ $(document).ready(function() {
             (text.indexOf(valThis) == 0) ? $(this).show(): $(this).hide();
         });
     });
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+ 
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
 });
