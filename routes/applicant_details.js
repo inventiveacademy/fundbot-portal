63f322a946f94df6e3432754aa133bfc957684b2
Trinity Siemer <tsiemer@gmail.com>
2017-09-28 17:33:07 -0500

    approve button update!

--- routes/applicant_details.js
+++ routes/applicant_details.js
@@ -12,6 +12,20 @@ router.get('/:id', function(req, res, next) {
     });
 });
 
+router.get('/:id', function(req, res, next) { 
+	console.log("testing approve button!", req.body);
+	console.log(req.params);
+    request({
+    		url: `http://localhost:3008/approveapplication/${req.params.id}`, 
+            type: "PUT",            
+            error: function(result){
+                console.log("Nooo💀️!!");
+            },
+            success: function(result){
+                console.log("Success!!");
+            }
+        }); 
+    res.redirect("/Applications_Overview"); 
 }); 
 
 module.exports = router;
