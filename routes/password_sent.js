c6ca1e667778e315e45ff5a1c9d1b2dafcfcde2c
Unknown <smashford12@gmail.com>
2017-09-27 19:18:09 -0500

    Added ability to link to login!

--- routes/password_sent.js
+++ routes/password_sent.js
@@ -4,11 +4,20 @@ var app = express();
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    request('http://localhost:3008/applications', function(error, response, body) {
-        let applications = JSON.parse(body);
-        res.render('password_sent', { title: 'Password Sent', applications });
+router.post('/', function(req, res, next) {
+    request({
+        url: 'http://localhost:3008/sendemail',
+        method: "POST",
+        json: {
+                 "from" : 'fundbot@inventive.io',
+                 "to" : req.body.email,
+                 "subject" : 'Hello! Is this working?',
+                 "text": 'You is Kind, You is Smart, You is Important'
+        }},
+        function(error, response, body) {
+        console.log("req.params",req.body);
+          
+        res.render('password_sent', { title: 'Password Sent'});
     });
 });
-
 module.exports = router;
